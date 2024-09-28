74456da77fb54559808ca2ddf3dfec98c52cc33c
alex <aschwarz@xephyr.xyz>
2024-09-28 17:31:29 -0500

    update model_id in app.py

--- ai/app.py
+++ ai/app.py
@@ -3,10 +3,11 @@ from transformers import AutoTokenizer
 from flask import Flask, request
 import json
 #prompt = "What is the school the University of Nebraska Lincoln?"
-model_id = "OpenVINO/Phi-3-mini-4k-instruct-int4-ov"
+model_id = "OpenVINO/Phi-3-mini-128k-instruct-int4-ov"
+print("Model: " + model_id)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 #inputs = tokenizer(prompt, return_tensors="pt")
-model = OVModelForCausalLM.from_pretrained("OpenVINO/Phi-3-mini-4k-instruct-int4-ov")
+model = OVModelForCausalLM.from_pretrained(model_id)
 #model.to("GPU")
 #outputs = model.generate(**inputs, max_new_tokens=50)
 
@@ -17,7 +18,7 @@ async def complete():
     data = request.get_json()
     request_prompt = data.get("prompt")
     inputs = tokenizer(request_prompt, return_tensors="pt")
-    outputs = model.generate(**inputs, max_new_tokens=150)
+    outputs = model.generate(**inputs, max_new_tokens=500)
     allOutputs = []
     for o in outputs:
         tokened = tokenizer.decode(o, skip_special_tokens=True)
@@ -25,4 +26,4 @@ async def complete():
         allOutputs.append(tokened)
     return json.dumps(allOutputs)
  
-api.run(host='0.0.0.0')
+api.run()
